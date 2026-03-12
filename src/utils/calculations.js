c322c2d41f36dd0d2819e7b044e571f3664957cc
Jacob Rosario <jacobcvrosario@gmail.com>
2026-03-12 03:47:48 -0700

    working on Results.jsx component

---
+++ src/utils/calculations.js
@@ -0,0 +1,36 @@
+
+export function calculateSpikeMetrics(takeoff, contact, landing) {
+
+    const airtime = landing - takeoff;
+    const idealContact = airtime / 2; // calculated
+    const actualContact = contact - takeoff; // expected
+    const spikingDifference = actualContact - idealContact;
+
+    // if the expected > calculated --> late
+    // if the expected < calculated --> early
+    // if the expected == calculated --> perfect
+
+    const accuracy = Math.max(0, (1- Math.abs(spikingDifference) / airtime) * 100);
+    // 100% = perfect hit
+    // Math.max(0) make sure we never go below 0%
+
+    return {
+        airtime: round(airtime),
+        idealContact: round(idealContact),
+        actualContact: round(actualContact),
+        spikingDifference: round(spikingDifference),
+        accuracy: round(accuracy)
+    }
+};
+
+export function getResultLabel(spikingDifference) {
+    const PERFECT_WINDOW = 0.05; // withing 50ms = perfect
+
+    if (Math.abs(spikingDifference) <= PERFECT_WINDOW) return "Perfect";
+    if (spikingDifference < 0) return "Early";
+    return "Late";
+}
+
+function round(n) {
+    return Math.round(n * 1000) / 1000; // 3 decimal places
+}
